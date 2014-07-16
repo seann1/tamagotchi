6ee9dbb0c0f2f5e424cc829bbff87be0148f5f29
Kyle Boe & Sean Niesen <student@epicodus.com>
2014-07-16 12:22:25 -0700

    added places in html for script data to be shown

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,46 @@
-// Project Scripts
+var Tamagotchi = {
+  initialize: function(name){
+    this.name = name
+    this.foodLevel = 10;
+    this.sleepLevel = 10;
+    this.activityLevel = 10;
+  },
+  name: "",
+  foodLevel: 0,
+  sleepLevel: 0,
+  activityLevel: 0,
+
+  timePasses: function(){
+
+    this.foodLevel --;
+    this.sleepLevel --;
+    this.activityLevel --;
+  },
+
+  isAlive: function(){
+    if (this.foodLevel > 0)
+      return true;
+    else
+      return false;
+  }
+};
+
+
+
+$(document).ready(function() {
+  $("form#petCreator").submit(function(event){
+     var newPet = Object.create(Tamagotchi);
+     var petName = $("input#petName").val();
+     newPet.initialize(petName);
+     $("input#petName").val("");
+     $("div#create-tom").hide();
+     $("div#tom").show();
+     $("#foodNum").text(newPet.foodLevel);
+     $("#sleepNum").text(newPet.sleepLevel);
+     $("#activityNum").text(newPet.activityLevel);
+     $("#putName").text(petName);
+
+
+     event.preventDefault();
+    });
+});
--- js/specs.js
+++ js/specs.js
@@ -1,1 +1,35 @@
-// Tests Go Here!
+describe("Tamagotchi", function() {
+  describe("initialize", function() {
+    it("sets the name and a few other properties", function() {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("lil dragon");
+      myPet.name.should.equal("lil dragon");
+      myPet.foodLevel.should.equal(10);
+      myPet.sleepLevel.should.equal(10);
+      myPet.activityLevel.should.equal(10);
+    });
+  });
+
+  describe("timePasses", function() {
+    it("decreases the amount of food the Tamogatchi has left by 1", function() {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize("lil dragon");
+      myPet.timePasses();
+      myPet.foodLevel.should.equal(9);
+    });
+  });
+
+  describe("isAlive", function() {
+    it("is alive if the food level is above 0", function() {
+      var myPet = Object.create(Tamagotchi);
+      myPet.initialize();
+      myPet.isAlive().should.equal(true);
+    });
+
+    it("is dead if the food level is 0", function() {
+      var myPet = Object.create(Tamagotchi);
+      myPet.foodLevel = 0;
+      myPet.isAlive().should.equal(false);
+    });
+  });
+});
